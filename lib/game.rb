857557bd54529893147edaf3f04cc981932fdb46
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-10 16:26:24 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -47,5 +47,18 @@ class Game
     end
   end
 
+  def turn
+    puts "Please ender a number 1-9:"
+    @user_input = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else  
+      puts "Please enter a number 1-9:"
+      @board.display
+      turn
+    end
+    @board.display
+  end
+
 
 end
