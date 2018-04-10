39439b465a0073d98a41fd761dfa932ba8ba1bf7
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-10 16:11:48 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,7 @@ class Game
     [2,4,6],
   ]
 
-  def method_name(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = board.new)
+  def method_name(player_1, player_2, board)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
