c4ae23b5cdaac8663fd095cc28f2886e58deb299
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-10 16:08:56 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,19 @@
 class Game
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  def method_name(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = board.new)  
+  end
+
+
 end
